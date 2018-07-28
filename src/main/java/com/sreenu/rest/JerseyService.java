c8672b4dbd41fc7eef35e5f93a17c997594e4030
AnilAleti <anil.javaws@gmail.com>
2018-07-28 11:59:21 +0530

    added rest resource class

---
+++ src/main/java/com/sreenu/rest/JerseyService.java
@@ -0,0 +1,14 @@
+package com.sreenu.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/healthCheck")
+public class JerseyService
+{
+    @GET
+    public String getMsg()
+    {
+         return "Jersey rest service running....";
+    }
+}
